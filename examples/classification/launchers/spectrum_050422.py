af56ffbe2c0dfd3b1c255888cdd0b15c1a220124
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-05-04 22:43:14 -0700

    Fix bug.

--- examples/classification/launchers/spectrum_050422.py
+++ examples/classification/launchers/spectrum_050422.py
@@ -13,7 +13,7 @@ def _get_cmd(random_init, dump_path):
 def main():
     cmds = []
     for random_init in (True, False):
-        dump_path = f"/mnt/disks/disk-2/dump/spectrum/init_compare/dump_{random_init}.json"
+        dump_path = f"/mnt/disks/disk-2/dump/spectrum/init_compare/dump_{random_init}.pt"
         cmds.append(_get_cmd(random_init=random_init, dump_path=dump_path))
     from swissknife import utils
     utils.gpu_scheduler(commands=cmds, excludeID=(0, 1), excludeUUID=(0, 1))
