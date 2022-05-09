6f6fe456ac0d06c50fd6557dcf30f27bed9510b8
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-05-09 16:22:51 -0700

    Refactor.

--- examples/decay_toy.py
+++ examples/decay_toy.py
@@ -10,18 +10,17 @@ import torch
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 torch.set_default_dtype(torch.float64)
 
-n = 5000
+n = 10000
 d = 100
 R = 1
 C = 1
 sum_sqrt = torch.sum(torch.arange(1, d + 1) ** -.5)  # sum_j 1 / sqrt(j)
-M_decay = 1 / 4 * C ** 2 / sum_sqrt
-M_const = 1 / 4 * C ** 2 / d
+G0 = 1 / 4 * C ** 2 / d
 
 beta_opt = torch.full(fill_value=.5 / math.sqrt(d), size=(d,), device=device)  # 2-norm == 0.5; within radius-R ball.
 mu_x = torch.zeros(size=(d,), device=device)
-si_x_decay = torch.sqrt(M_decay * torch.arange(1, d + 1, device=device) ** -.5)  # standard deviation.
-si_x_const = torch.full(fill_value=M_const, size=(d,), device=device)
+si_x_decay = math.sqrt(G0) * torch.sqrt(torch.arange(1, d + 1, device=device) ** -.5)  # standard deviation.
+si_x_const = math.sqrt(G0) * torch.ones(size=(d,), device=device)
 sensitivity = 2 / n * C ** 2 * R
 
 # Per-step epsilon and delta.
@@ -63,14 +62,16 @@ def evaluate(x, y, beta):
 @torch.no_grad()
 def train(x, y, num_steps, eval_steps, lr):
     beta = torch.zeros(size=(d,))
+    beta_avg = beta.clone()
     for global_step in range(1, num_steps + 1):
         if global_step % eval_steps == 0:
-            mse, dis = evaluate(x=x, y=y, beta=beta)
+            mse, dis = evaluate(x=x, y=y, beta=beta_avg)
             logging.warning(f"global_step: {global_step}, mse: {mse:.6f}, iterate dist: {dis:.6f}")
         beta = train_one_step(x=x, y=y, beta=beta, lr=lr)
+        beta_avg = beta_avg * global_step / (global_step + 1) + beta / (global_step + 1)
 
 
-def main(num_steps=100, eval_steps=10, lr=1):
+def main(num_steps=1000, eval_steps=50, lr=0.1):
     x, y = make_data(mode="decay")
     train(x, y, num_steps=num_steps, eval_steps=eval_steps, lr=lr)
 
