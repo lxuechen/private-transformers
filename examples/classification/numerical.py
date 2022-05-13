8a7979bb3679506ce74b7262767907e30b6c1429
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-05-12 17:35:54 -0700

    Fix bugs and add tqdm.

--- examples/classification/numerical.py
+++ examples/classification/numerical.py
@@ -31,7 +31,7 @@ def qr(grads_dir="/mnt/disks/disk-2/dump/classification/test/grad_trajectory", n
     Q = get_bases(data=data, k=500, num_power_iteration=2)
 
 
-def get_bases(data: torch.Tensor, k: int, num_power_iteration=1, save_mem=True, enable_tqdm=True, verbose=True):
+def get_bases(data: torch.Tensor, k: int, num_power_iteration=1, save_mem=True, disable_tqdm=False, verbose=True):
     """QR algorithm for finding top-k eigenvectors.
 
     Args:
@@ -39,7 +39,7 @@ def get_bases(data: torch.Tensor, k: int, num_power_iteration=1, save_mem=True, 
         k: Number of principal components to return.
         num_power_iteration: Number of power iterations.
         save_mem: If True, perform matmul in a for loop to save memory.
-        enable_tqdm: If True, report progress bar for power iteration.
+        disable_tqdm: If True, disable progress bar.
         verbose: If True, log the error of QR.
 
     Returns:
@@ -47,28 +47,31 @@ def get_bases(data: torch.Tensor, k: int, num_power_iteration=1, save_mem=True, 
         error_rate: Tensor of size (1,) for relative tolerance.
     """
     n, p = data.size()
-    k = min(k, p)
-    Q = torch.randn(size=(p, k), device=data.device)
-    for _ in tqdm.tqdm(range(num_power_iteration), desc="power iter", disable=not enable_tqdm):
+    k = min(k, p, n)
+    Q = torch.randn(size=(p, k))
+    for _ in tqdm.tqdm(range(num_power_iteration), desc="power iter", disable=disable_tqdm):
         if save_mem:
-            R = torch.stack([data @ Q[:, col_idx] for col_idx in range(k)], dim=1)
-            Q = torch.stack([data.t() @ R[:, col_idx] for col_idx in range(k)], dim=1)
+            # TODO: Chunk this.
+            iterator = tqdm.tqdm(range(k), desc="R", disable=disable_tqdm)
+            R = torch.stack([(data @ Q[:, col_idx].to(device)).cpu() for col_idx in iterator], dim=1)
+            iterator = tqdm.tqdm(range(k), desc="Q", disable=disable_tqdm)
+            Q = torch.stack([(data.T @ R[:, col_idx].to(device)).cpu() for col_idx in iterator], dim=1)
         else:
             R = torch.matmul(data, Q)  # np, pk -> nk.
-            Q = torch.matmul(data.t(), R)  # pn, nk -> pk.
-        _orthogonalize(Q)  # pk; orthonormalize the columns.
+            Q = torch.matmul(data.T, R)  # pn, nk -> pk.
+        _orthogonalize(matrix=Q, disable_tqdm=disable_tqdm)  # pk; orthonormalize the columns.
 
     if verbose:
-        err_abs, err_rel = _check_qr_error(data=data, Q=Q, save_mem=save_mem)
+        err_abs, err_rel = _check_qr_error(data=data, Q=Q, save_mem=save_mem, disable_tqdm=disable_tqdm)
         logging.warning(f"abs error: {err_abs:.6f}, rel error: {err_rel:.6f}")
 
     return Q
 
 
-def _orthogonalize(matrix):
+def _orthogonalize(matrix, disable_tqdm: bool):
     """Gram-Schmidt."""
     n, m = matrix.size()
-    for i in range(m):
+    for i in tqdm.tqdm(range(m), desc="orthogonalize", disable=disable_tqdm):
         # Normalize the ith column.
         col = matrix[:, i: i + 1]
         col /= col.norm(2)
@@ -78,12 +81,20 @@ def _orthogonalize(matrix):
             rest -= torch.sum(col * rest, dim=0) * col
 
 
-def _check_qr_error(data: torch.Tensor, Q: torch.Tensor, save_mem: bool):
+def _check_qr_error(data: torch.Tensor, Q: torch.Tensor, save_mem: bool, disable_tqdm: bool):
     n, p = data.size()
     _, k = Q.size()
+    Q = Q.cpu()
+
     if save_mem:
-        recon = torch.stack([data @ Q[:, col_idx] for col_idx in range(k)], dim=1)  # nk.
-        recon = torch.stack([recon @ Q[col_idx, :] for col_idx in range(p)], dim=1)  # np.
+        iterator = tqdm.tqdm(range(k), desc="check qr:: encode", disable=disable_tqdm)
+        data_mul_Q = torch.cat([(data.matmul(Q[:, idx][:, None].to(device))) for idx in iterator], dim=1)  # nk.
+
+        data_mul_Q = data_mul_Q.cpu()
+        Q = Q.to(device)
+
+        iterator = tqdm.tqdm(range(n), desc="check qr:: decode", disable=disable_tqdm)
+        recon = torch.cat([(Q.matmul(data_mul_Q[idx][:, None].to(device))) for idx in iterator], dim=1).T  # np.
     else:
         recon = data @ Q @ Q.T  # np.
 
