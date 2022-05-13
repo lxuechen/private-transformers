26b8a9ebf6b21a74c7219f552cafa57e479d8e64
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-05-12 17:41:02 -0700

    Fast orthogonalization.

--- examples/classification/numerical.py
+++ examples/classification/numerical.py
@@ -64,7 +64,7 @@ def get_bases(data: torch.Tensor, k: int, num_power_iteration=1, save_mem=True, 
         else:
             R = torch.matmul(data, Q)  # np, pk -> nk.
             Q = torch.matmul(data.T, R)  # pn, nk -> pk.
-        _orthogonalize(matrix=Q, disable_tqdm=disable_tqdm)  # pk; orthonormalize the columns.
+        Q = _orthogonalize(matrix=Q, disable_tqdm=disable_tqdm)  # pk; orthonormalize the columns.
 
     if verbose:
         err_abs, err_rel = _check_qr_error(data=data, Q=Q, save_mem=save_mem, disable_tqdm=disable_tqdm)
@@ -78,6 +78,9 @@ def _orthogonalize(matrix, disable_tqdm: bool):
 
     By far the slowest step, since cannot be parallelized.
     """
+    default_device = matrix.device
+
+    matrix = matrix.to(device)
     n, m = matrix.size()
     for i in tqdm.tqdm(range(m), desc="orthogonalize", disable=disable_tqdm):
         # Normalize the ith column.
@@ -87,6 +90,8 @@ def _orthogonalize(matrix, disable_tqdm: bool):
         if i + 1 < m:
             rest = matrix[:, i + 1:]
             rest -= torch.sum(col * rest, dim=0) * col
+
+    return matrix.to(default_device)
 
 
 def _check_qr_error(data: torch.Tensor, Q: torch.Tensor, save_mem: bool, disable_tqdm: bool):
